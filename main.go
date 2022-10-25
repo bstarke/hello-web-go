bd9f2414457353a239000c09bdd9297ee9b86002
Brad Starkenberg <brstarkenberg.ctr@kr.af.mil>
2022-10-25 10:01:05 -0500

    add mux

--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	mux := routes()
+	http.ListenAndServe(":8080", mux)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func routes() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/time`, timeHandler)
+	mux.HandleFunc(`/name/`, helloServer)
+	mux.HandleFunc(`/`, helloWorld)
+	//mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
+	return mux
+}
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[6:])
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(time.RFC1123)
+	w.Write([]byte("The time is: " + tm))
 }
